ed726440fdc719d8450eb0a5225ce498640eec3d
Daniyal Ayaz <106926956+universdynamics-daniyalayyaz@users.noreply.github.com>
2022-06-24 00:33:09 +0500

    Latest

--- BusinessDashboard.js
+++ BusinessDashboard.js
@@ -1,5 +1,5 @@
 import { Avatar, Button, Card, Checkbox, Paragraph } from 'react-native-paper';
-import { StyleSheet, Text, View, TextInput, Drawer, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Drawer, SafeAreaView, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function BusinessDashboard() {
@@ -14,17 +14,17 @@ export default function BusinessDashboard() {
         navigation.navigate('Booking Options')
     }
     return (
-        <SafeAreaView style={{height: '100%'}}>
+        <SafeAreaView >
             <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%', backgroundColor: '#ffe4e4', }}>
                 <Card style={{ padding: '40px', borderRadius: '20px', width: '90%', marginBottom: '30px', marginTop: '30px' }}>
-                    <View style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Image source={require('./assets/booking.gif')} style={{width: '100%', height: '300px'}}></Image>
                         <Text style={{ fontWeight: 'bold', fontSize: '24px', marginBottom: '10px' }}>Bookings</Text>
                         <Text style={{ fontWeight: 'bold', fontSize: '16px', marginBottom: '10px' }}>View Booking Schedules and Requests</Text>
                         <Button mode='contained' style={{backgroundColor: '#FF69B4'}} onPress={()=>movess()}>Manage</Button>
-                    </View>
                 </Card>
                 <Card style={{ padding: '40px', borderRadius: '20px', width: '90%', marginBottom: '30px' }}>
                     <View style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Image source={require('./assets/service.gif')} style={{width: '100%', height: '300px'}}></Image>
                         <Text style={{ fontWeight: 'bold', fontSize: '24px', marginBottom: '10px' }}>Services</Text>
                         <Text style={{ fontWeight: 'bold', fontSize: '16px', marginBottom: '10px' }}>View and Update your services</Text>
                         <Button mode='contained' style={{backgroundColor: '#FF69B4'}} onPress={()=>move()}>Manage</Button>
@@ -32,6 +32,7 @@ export default function BusinessDashboard() {
                 </Card>
                 <Card style={{ padding: '40px', borderRadius: '20px', width: '90%', marginBottom: '30px' }}>
                     <View style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Image source={require('./assets/settings.gif')} style={{width: '100%', height: '300px'}}></Image>
                         <Text style={{ fontWeight: 'bold', fontSize: '24px', marginBottom: '10px' }}>Profile Settings</Text>
                         <Text style={{ fontWeight: 'bold', fontSize: '16px', marginBottom: '10px' }}>Keep your Profile Information Up to Date</Text>
                         <Button mode='contained' style={{backgroundColor: '#FF69B4'}} onPress={()=>moves()}>Manage</Button>
